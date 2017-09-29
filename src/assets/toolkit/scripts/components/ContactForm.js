985d2ec1d86417841206c6d7b08be9a3c673246e
Benoît Burgener <benoit.burgener@liip.ch>
2017-09-29 17:28:15 +0200

    Improve contact form experience regarding open/close details

--- src/assets/toolkit/scripts/components/ContactForm.js
+++ src/assets/toolkit/scripts/components/ContactForm.js
@@ -6,7 +6,11 @@ export default class ContactForm {
   constructor(contactForm) {
     this._contactForm = contactForm;
     this._contactFormStepTwo = this._contactForm.querySelector('.contact-form__step-2');
+    this._contactFormName = this._contactForm.querySelector('.contact-form__name');
+    this._contactFormEmail = this._contactForm.querySelector('.contact-form__email');
     this._contactFormMessage = this._contactForm.querySelector('.contact-form__message');
+    this._fields = [this._contactFormName, this._contactFormEmail, this._contactFormMessage];
+    this._isOpen = false;
 
     this.collapse = new Collapse(this._contactFormStepTwo);
     this.expanding = new Expanding(this._contactFormMessage);
@@ -15,11 +19,28 @@ export default class ContactForm {
   }
 
   _addEventListeners() {
-    this._contactFormMessage.addEventListener('focus', this.collapse.show);
-    this._contactFormMessage.addEventListener('blur', () => {
-      if (this._contactFormMessage.value.length === 0) {
-        this.collapse.hide();
-      }
+    this._fields.forEach(field => {
+      field.addEventListener('focus', () => {
+        this._isOpen = true;
+        this.collapse.show();
+      });
+
+      field.addEventListener('blur', () => {
+        const data = this._fields.reduce((counter, field) => {
+          return counter += field.value.length;
+        }, 0);
+
+        // Keep the form details open if any field was filled-in
+        if (data === 0) {
+          this._isOpen = false;
+          setTimeout(() => {
+            // In case we did not focus another field in-between, hide the details
+            if (!this._isOpen) {
+              this.collapse.hide();
+            }
+          }, 100);
+        }
+      });
     });
   }
 
